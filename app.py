f8d9ae1e5fd652e1c91004d2c997f0f9fc4af30b
Kay Alave <k.alave@gmail.com>
2025-08-04 13:37:54 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys, os
+from flask import Flask
+
+
+app = Flask(__name__)# URL Routing — Home Page
+
+@app.route('/')
+def index():
+    return "Welcome to the Flask App!"
+
+if __name__ == '__main__':
+    # Set the environment variable for Flask
+    os.environ['FLASK_APP'] = 'app.py'
+    
+    # Run the Flask application
+    app.run(port=8000)
+
+
+            
